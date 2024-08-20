9f1e6d91f9b656fedab3e2cf2b33a8932e1a777b
Nikita Skurenko <surmans51@gmail.com>
2024-08-20 17:08:56 +0600

    SecondCommit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Phone iphone1 = new Phone(1,2,3);
+
+       /* Phone iphone1 = new Phone(1,2,3);
         Phone iphone2 = new Phone(3,4);
         Phone iphone3 = new Phone();
 
@@ -34,7 +37,37 @@ public class Main {
         person1.move("nik");
         person1.talk("nikao");
         person.talk("vade");
-        person.move("loax");
+        person.move("loax"); */
 
+        /*String [] infoMassiv = new String[5];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vvedite FIO:");
+        String fIO = sc.nextLine();
+        infoMassiv[0] = fIO;
+
+        System.out.println("Vvedite nomer chitatelskogo bileta:");
+        String numberBook = sc.nextLine();
+        infoMassiv[1] = numberBook;
+
+        System.out.println("Vvedite nazvanie faculty:");
+        String faculty = sc.nextLine();
+        infoMassiv[2] = faculty;
+
+        System.out.println("Vvedite daty rozdenia:");
+        String dateBorn = sc.nextLine();
+        infoMassiv[3] = dateBorn;
+
+        System.out.println("Vvdeite nomer phone:");
+        String numberPhone = sc.nextLine();
+        infoMassiv[4] = numberPhone;
+
+        for (int i = 0; i < infoMassiv.length; i++) {
+            System.out.print(infoMassiv[i] + " ");
+        }*/
+        Matrix matrix = new Matrix();
+        //Matrix.scannMatrix();
+        //matrix.sumMatrix();
+        matrix.printMatrix();
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,10 +1,68 @@
-public class Matrix {
-    double[][] dvoinoiMassiv;
-    int lines;
-    int cols;
+import java.util.Scanner;
 
-    void printMassiv() {
-        System.out.println("Razmernost massiva: ");
+public class Matrix {
+
+     int row = scannMatrixRowAndCol();
+     int col = scannMatrixRowAndCol();
+
+    int doubleMatrix [][] = scannMatrix();
+    int doubleMatrixDva[][] = scannMatrix();
+
+    int scannMatrixRowAndCol() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int par = scanner.nextInt();
+        return par;
     }
+
+     int[][] scannMatrix() {
+        int testMatrix[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            System.out.println();
+            for (int j = 0; j < col; j++) {
+                 testMatrix[i][j] = (int) (Math.random() * (100+1)) - 50;
+                 //System.out.print(testMatrix[i][j] + " ");
+            }
+        }
+        return testMatrix;
+    }
+
+    void printMatrix() {
+       for (int i = 0; i < row; i++) {
+           System.out.println();
+           for (int j = 0; j < col; j++) {
+               System.out.print(doubleMatrix[i][j] + " ");
+           }
+       }
+       System.out.println();
+
+        for (int i = 0; i < row; i++) {
+            System.out.println();
+            for (int j = 0; j < col; j++) {
+                System.out.print(doubleMatrixDva[i][j] + " ");
+            }s
+        }
+   }
+
+   int[][] sumMatrix() {
+        int sumMatrix[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                sumMatrix[i][j] = doubleMatrix[i][j] + doubleMatrixDva[i][j];
+                System.out.println(sumMatrix[i][j] + " ");
+            }
+        }
+        return sumMatrix;
+    }
+
+    void printMatrix(int[][] doubleMatrix, int[][] doubleMatrixDva) {
+       for (int i = 0; i < row; i++) {
+           for (int j = 0; j < col; j++) {
+               //int sumMatrix = sumMatrix(doubleMatrix[][], doubleMatrix[][]);
+               System.out.print(doubleMatrix[i][j] + " ");
+           }
+       }
+    }
+
 
 }
